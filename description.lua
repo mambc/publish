a64901da1aeb0d3c9e9a0caef00f29b06aab1ef4
Pedro Andrade <pedro.andrade@inpe.br>
2016-08-29 09:26:59 -0300

    First version of description.lua.

---
+++ description.lua
@@ -0,0 +1,9 @@
+version = "0.1"
+license = "LGPL-3"
+package = "publish"
+title = "Publishing Model Results"
+url = "https://github.com/pedro-andrade-inpe/publish"
+authors = "Heitor Carneiro and Pedro R. Andrade"
+contact = "heitorguerrac@gmail.com, pedro.andrade@inpe.br"
+content = "This package allows one to export the outputs of different Model scenarios to a web server."
+
