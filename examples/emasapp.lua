25f22a57c2ad1223d6b69827db29750be74c450f
hguerra <heitorguerrac@gmail.com>
2016-10-19 16:19:28 -0200

    Adding README and emasapp.lua

--- examples/emasapp.lua
+++ examples/emasapp.lua
@@ -1,24 +1,23 @@
--------------------------------------------------------------------------------------------
--- TerraME - a software platform for multiple scale spatially-explicit dynamic modeling.
--- Copyright (C) 2001-2016 INPE and TerraLAB/UFOP -- www.terrame.org
+-- @example Implementation of a simple emas application. It uses the data from a tview file (terralib/emas.tview).
+-- There is a layout with the web page properties.
+-- Each polygon is drawn using the attribute 'select'.
 
--- This code is part of the TerraME framework.
--- This framework is free software; you can redistribute it and/or
--- modify it under the terms of the GNU Lesser General Public
--- License as published by the Free Software Foundation; either
--- version 2.1 of the License, or (at your option) any later version.
+import("publish")
 
--- You should have received a copy of the GNU Lesser General Public
--- License along with this library.
+local layout = Layout{
+	title = "Emas",
+	description = "Creates a database that can be used by the example fire-spread of base package.",
+	base = "satellite",
+	zoom = 10,
+	center = {lat = -18.106389, long = -52.927778}
+}
 
--- The authors reassure the license terms regarding the warranties.
--- They specifically disclaim any warranties, including, but not limited to,
--- the implied warranties of merchantability and fitness for a particular purpose.
--- The framework provided hereunder is on an "as is" basis, and the authors have no
--- obligation to provide maintenance, support, updates, enhancements, or modifications.
--- In no event shall INPE and TerraLAB / UFOP be held liable to any party for direct,
--- indirect, special, incidental, or consequential damages arising out of the use
--- of this software and its documentation.
---
--------------------------------------------------------------------------------------------
-
+Application{
+	project = filePath("emas.tview", "terralib"),
+	layout = layout,
+	clean = true,
+	select = "river",
+	color = "BuGn",
+	value = {0, 1, 2},
+	output = Directory("EmasWebMap")
+}
