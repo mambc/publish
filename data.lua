8b8229f9772079c46caebfe7b94b1f4c167e151a
hguerra <heitorguerrac@gmail.com>
2016-11-11 13:56:38 -0200

    #37. Adding data.lua

---
+++ data.lua
@@ -0,0 +1,84 @@
+data{
+	file = "caragua.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		population = "Population of Caraguatatuba.",
+		pib = "Gross Domestic Product (GDP) of Caraguatatuba.",
+		name = "Data name.",
+		idh = "HDI indicators of Caraguatatuba."
+	}
+}
+
+data{
+	file = "regions.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		name = "Regions of Caraguatatuba.",
+	}
+}
+
+data{
+	file = "caragua_classes2010_regioes.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Social Classe 2010 Real.",
+	}
+}
+
+data{
+	file = "occupational2010.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		uso = "Occupational Classes (IBGE, 2010).",
+	}
+}
+
+data{
+	file = "simulation2025_baseline.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Social Classes 2025 Simulated.",
+	}
+}
+
+data{
+	file = "simulation2025_lessgrowth.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Urban Population Lessgrowth 2025.",
+	}
+}
+
+data{
+	file = "simulation2025_plusgrowth.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Urban Population Plusgrowth 2025.",
+	}
+}
+
+data{
+	file = "simulation2025_poorer.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Socioeconomic Status Poorer 2025.",
+	}
+}
+
+data{
+	file = "simulation2025_richer_final.shp",
+	summary = "",
+	source = "",
+	attributes = {
+		classe = "Socioeconomic Status Richer 2025.",
+	}
+}
+
