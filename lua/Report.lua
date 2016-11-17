1dcd821eab78271742d2992f5ca5998d2df491c0
hguerra <heitorguerrac@gmail.com>
2016-11-17 10:14:13 -0200

    #11. Adding initial implementation of Report.

--- lua/Report.lua
+++ lua/Report.lua
@@ -21,3 +21,77 @@
 -- of this software and its documentation.
 --
 -------------------------------------------------------------------------------------------
+
+Report_ = {
+	type_ = "Report",
+	addImage = function(self, image, package)
+		if type(image) == "string" then
+			if package then
+				if type(package) == "string" then
+					image = packageInfo(package).path.."images/"..image
+				else
+					incompatibleTypeError(2, "string", package)
+				end
+			end
+
+			image = File(image)
+		end
+
+		mandatoryArgument(1, "File", image)
+
+		if image:exists() then
+			local extension = image:extension():lower()
+			if belong(extension, {"bmp", "gif", "jpeg", "jpg", "png", "svg"}) then
+				table.insert(self.image, image)
+			else
+				customError("'"..extension.."' is an invalid extension for argument 'image'. Valid extensions ['bmp', 'gif', 'jpeg', 'jpg', 'png', 'svg'].")
+			end
+		else
+			resourceNotFoundError("image", tostring(image))
+		end
+	end,
+	--- Add a line in the report
+	-- @usage import("publish")
+	-- local report = Report()
+	-- report:separator()
+	addSeparator = function(self)
+		table.insert(self.separator, #self.text + 1)
+	end,
+	addText = function(self, text)
+		mandatoryArgument(1, "string", text)
+		table.insert(self.text, text)
+	end,
+	setTitle = function(self, title)
+		mandatoryArgument(1, "string", title)
+		self.title = title
+	end
+}
+
+metaTableReport_ = {
+	__index = Report_,
+	__tostring = _Gtme.tostring
+}
+
+--- Creates a page with data information.
+-- @arg data.title An optional string with the name of the title of the report.
+-- @arg data.text An optional string with the text to the report.
+-- @arg data.image An optional string or File with the image of the report.
+function Report(data)
+	data = data or {}
+	mandatoryArgument(1, "table", data)
+	verifyUnnecessaryArguments(data, {"title", "text", "image"})
+	optionalTableArgument(data, "title", "string")
+
+	local mdata = {title = data.title, text = {}, image = {}, separator = {}}
+	setmetatable(mdata, metaTableReport_)
+
+	if data.text then
+		mdata:addText(data.text)
+	end
+
+	if data.image then
+		mdata:addImage(data.image)
+	end
+
+	return mdata
+end
