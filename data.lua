c459ba4384a8e03b3b154f2220e74f520e216b47
hguerra <heitorguerrac@gmail.com>
2016-11-17 10:56:27 -0200

    #11. Adding doc of Report.

--- data.lua
+++ data.lua
@@ -14,6 +14,7 @@ data{
 	file = "regions.shp",
 	summary = "Regions of Caraguatatuba.",
 	source = "IBGE (http://www.ibge.gov.br)",
+	image = "urbis_regions.png",
 	attributes = {
 		name = "Regions of Caraguatatuba, North (1), Central (2) and South (3).",
 	}
@@ -23,6 +24,7 @@ data{
 	file = "caragua_classes2010_regioes.shp",
 	summary = "Social Classe 2010 Real.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_2010_real.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
@@ -32,6 +34,7 @@ data{
 	file = "occupational2010.shp",
 	summary = "Occupational Classes (IBGE, 2010).",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_uso_2010.PNG",
 	attributes = {
 		uso = "The percentage of houses and apartments inside such areas that is typically used in summer vacations and holidays.",
 	}
@@ -41,6 +44,7 @@ data{
 	file = "simulation2025_baseline.shp",
 	summary = "Social Classes 2025 Simulated.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_simulation_2025_baseline.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
@@ -50,6 +54,7 @@ data{
 	file = "simulation2025_lessgrowth.shp",
 	summary = "Urban Population Lessgrowth 2025.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_simulation_2025_lessgrowth.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
@@ -59,6 +64,7 @@ data{
 	file = "simulation2025_plusgrowth.shp",
 	summary = "Urban Population Plusgrowth 2025.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_simulation_2025_plusgrowth.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
@@ -68,6 +74,7 @@ data{
 	file = "simulation2025_poorer.shp",
 	summary = "Socioeconomic Status Poorer 2025.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_simulation_2025_poorer.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
@@ -77,6 +84,7 @@ data{
 	file = "simulation2025_richer_final.shp",
 	summary = "Socioeconomic Status Richer 2025.",
 	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
+	image = "urbis_simulation_2025_richer.PNG",
 	attributes = {
 		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
