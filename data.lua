0d0ef5d70ec52e30117311aa37574022a8cbed5c
hguerra <heitorguerrac@gmail.com>
2016-11-23 08:37:41 -0200

    #56. Updating data.lua

--- data.lua
+++ data.lua
@@ -1,7 +1,7 @@
 data{
 	file = "caragua.shp",
-	summary = "",
-	source = "",
+	summary = "Caraguatatuba statistical information.",
+	source = "IBGE (http://www.ibge.gov.br)",
 	attributes = {
 		population = "Population of Caraguatatuba.",
 		pib = "Gross Domestic Product (GDP) of Caraguatatuba.",
@@ -12,73 +12,73 @@ data{
 
 data{
 	file = "regions.shp",
-	summary = "",
-	source = "",
+	summary = "Regions of Caraguatatuba.",
+	source = "IBGE (http://www.ibge.gov.br)",
 	attributes = {
-		name = "Regions of Caraguatatuba.",
+		name = "Regions of Caraguatatuba, North (1), Central (2) and South (3).",
 	}
 }
 
 data{
 	file = "caragua_classes2010_regioes.shp",
-	summary = "",
-	source = "",
+	summary = "Social Classe 2010 Real.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Social Classe 2010 Real.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
 data{
 	file = "occupational2010.shp",
-	summary = "",
-	source = "",
+	summary = "Occupational Classes (IBGE, 2010).",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		uso = "Occupational Classes (IBGE, 2010).",
+		uso = "The percentage of houses and apartments inside such areas that is typically used in summer vacations and holidays.",
 	}
 }
 
 data{
 	file = "simulation2025_baseline.shp",
-	summary = "",
-	source = "",
+	summary = "Social Classes 2025 Simulated.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Social Classes 2025 Simulated.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
 data{
 	file = "simulation2025_lessgrowth.shp",
-	summary = "",
-	source = "",
+	summary = "Urban Population Lessgrowth 2025.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Urban Population Lessgrowth 2025.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
 data{
 	file = "simulation2025_plusgrowth.shp",
-	summary = "",
-	source = "",
+	summary = "Urban Population Plusgrowth 2025.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Urban Population Plusgrowth 2025.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
 data{
 	file = "simulation2025_poorer.shp",
-	summary = "",
-	source = "",
+	summary = "Socioeconomic Status Poorer 2025.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Socioeconomic Status Poorer 2025.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
 data{
 	file = "simulation2025_richer_final.shp",
-	summary = "",
-	source = "",
+	summary = "Socioeconomic Status Richer 2025.",
+	source = "Feitosa et. al (2014) URBIS-Caraguá: Um Modelo de Simulação Computacional para a Investigação de Dinâmicas de Ocupação Urbana em Caraguatatuba, SP.",
 	attributes = {
-		classe = "Socioeconomic Status Richer 2025.",
+		classe = "Categorizes the social conditions of households in Caraguatatuba on condition A (3), B (2) or C (1).",
 	}
 }
 
