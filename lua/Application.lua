e740e03462846c07e7d037ef8a2882f491d335ea
miriam-geoambiente <miriam.carvalho@geoambiente.com.br>
2017-12-12 09:28:34 -0200

    Close #1. Adding example of scenario.

--- lua/Application.lua
+++ lua/Application.lua
@@ -665,7 +665,7 @@ local function loadViewValue(data, name, view)
 			end
 
 			if data.scenario then
-				for scene, params in pairs(data.temporalConfig[name].scenario) do
+				for _, params in pairs(data.temporalConfig[name].scenario) do
 					for _, layerName in ipairs(params.name) do
 						loadValuesFromDataSet(set, data.project, layerName, select, mview.slices)
 					end
@@ -1219,8 +1219,9 @@ local function createApplicationProjects(data, proj)
 	end)
 
 	local scenarios
+	local hasScenario = false
 	if data.scenario then
-		scenarios = {{scenario = "None", view = ""} }
+		scenarios = {{scenario = "None", view = ""}}
 		forEachElement(mview, function(viewName, viewAttributes)
 			if not viewAttributes.scenario then return end
 
@@ -1232,6 +1233,8 @@ local function createApplicationProjects(data, proj)
 		table.sort(scenarios, function(k1, k2)
 			return k1.scenario < k2.scenario
 		end)
+
+		hasScenario = true
 	end
 
 	if data.bounds then
@@ -1272,7 +1275,8 @@ local function createApplicationProjects(data, proj)
 			logo = data.logo,
 			wms = data.hasWMS,
 			slider = getn(data.temporal) > 0,
-			scenarios = scenarios
+			scenarios = scenarios,
+			hasScenario = hasScenario
 		}
 	}
 end
@@ -1545,7 +1549,7 @@ function Application(data)
 			end
 
 			if not endswith(description, dot) then
-				description = description..dot
+				data.scenario[scenario] = description..dot
 			end
 		end)
 	end
